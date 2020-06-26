eada997f128defd4f7086298666a4a02c369aac5
Igor <igorretesz@gmail.com>
2020-06-26 01:14:24 +0100

    More features added✨
    Starting to add data base

--- data/db/build.sql
+++ data/db/build.sql
@@ -1,6 +1,6 @@
-CREATE TABLE IF NOT EXISTS exp(
-    UserID interger PRIMARY KEY,
-    XP interger DEFAULT 0,
-    Level interger DEFAULT 0,
-    XpPLock text DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE IF NOT EXISTS exp (
+	UserID integer PRIMARY KEY,
+	XP integer DEFAULT 0,
+	Level integer DEFAULT 0,
+	XPLock text DEFAULT CURRENT_TIMESTAMP
 );
--- lib/bot/__init__.py
+++ lib/bot/__init__.py
@@ -1,8 +1,11 @@
 from discord.ext.commands import Bot as BotBase
 from discord import Embed, File
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime
-from discord.exc.commands import CommandNotFound
+from discord.ext.commands import CommandNotFound
+from ..db import db
+
 
 PREFIX = '+'
 OWNER_IDS =[234472372491517952]
@@ -12,7 +15,10 @@ class Bot(BotBase):
         self.PREFIX = PREFIX
         self.ready = False
         self.guild = None
-        self.scheduler = AsyncIOScheduler
+        self.scheduler = AsyncIOScheduler()
+
+        db.autosave(self.scheduler)
+
         super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
 
     def run(self, version):
@@ -21,6 +27,9 @@ class Bot(BotBase):
             self.TOKEN = tf.read()
         print('Running bot ...')
         super().run(self.TOKEN, reconnect=True)
+
+    async def rules_reminder(self):
+        pass
 
     async def on_connect(self):
         print('Bot is connected')
@@ -46,25 +55,27 @@ class Bot(BotBase):
     async def on_ready(self):
         if not self.ready:
             self.ready = True
-            print('bot ready')
+            self.scheduler.add_job(self.rules_reminder, CronTrigger(day_of_week=0, hour=12, minute=0, second=0))
+            self.scheduler.start()
+
+
 
             channel = self.get_channel(693244168725856308)
             await channel.send("Now Online")
 
-            embed = Embed(title='Now Online!', description='Dashy-3 Is now online',
-                          colour=0xFF0000, timestamp=datetime.utcnow())
-            fields=[('Name','Value',True),
-                    ('Another Fireld','Another Value',True),
-                    ('A none inline filed','This fireld will apaer on new row same row',False),]
-            for name,value,inline in fields:
-                embed.add_field(name=name, value=value,inline=inline)
-                embed.set_author(name='Dashy-3')
-
-                embed.set_footer(text='This is a footer!')
-            await channel.send(embed=embed)
+            #embed = Embed(title='Now Online!', description='Dashy-3 Is now online',
+            #              colour=0xFF0000, timestamp=datetime.utcnow())
+            #fields=[('Name','Value',True),
+            #        ('Another Fireld','Another Value',True),
+            #        ('A none inline filed','This fireld will apaer on new row same row',False),]
+            #for name,value,inline in fields:
+            #    embed.add_field(name=name, value=value,inline=inline)
+            #    embed.set_author(name='Dashy-3')
+            #    embed.set_footer(text='This is a footer!')
+            #await channel.send(embed=embed)
             #await channel.send(file=File('./data/images/bot-icon-2883144_1280.png'))
 
-
+            print('bot ready')
         else:
             print('Bot reconnected')
 
